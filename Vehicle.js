e02348c6ddb5fe186ed93ced5c9da4836a7663ab
Tony Weidel <8aweidel@stpatrick.org>
2017-08-29 14:46:18 -0500

    so far so good

---
+++ Vehicle.js
@@ -0,0 +1,13 @@
+var vehicle = function(){
+    let engine = 6;
+    let fuel = 0
+    let gastank = 13;
+    let averageMPG = 22;
+    let milesDriven = 0;
+
+    function totalMiles(){
+        let totalMiles = 0;
+
+        if(milesDriven ==0
+    }
+}
