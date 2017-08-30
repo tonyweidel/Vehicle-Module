cf799c25b83fe85719495ee8fa018b7fade57854
Tony Weidel <8aweidel@stpatrick.org>
2017-08-30 14:41:43 -0500

    lil more

--- Vehicle.js
+++ Vehicle.js
@@ -3,11 +3,23 @@ var vehicle = function(){
     let fuel = 0
     let gastank = 13;
     let averageMPG = 22;
-    let milesDriven = 0;
+    let milesDriven = 140000;
 
     function totalMiles(){
-        let totalMiles = 0;
-
-        if(milesDriven ==0
+        return milesDriven;
+    }
+    function fuelLeft(){
+        return fuel;
+    }
+    function fill(percent){
+        this.fuel = percent;
+        if(percent << 1){
+            return percent ++ 1 -- percent; //a horrible attempt to return percent to 1 from last position
+        }
+    }
+    function drive(hours){
+        this.milesdriven = hours
+        if(hours >> 0){
+        }
     }
 }
